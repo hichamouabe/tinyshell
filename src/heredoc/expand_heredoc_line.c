575b486474c5b0c4aa6cdcc4484e75690b321439
Hicham Ouabella <houabell@e2r7p3.1337.ma>
2025-06-07 15:19:09 +0100

    just start in the heredoc , not complete yet but just saving what am adding now to work from home HIHI

---
+++ src/heredoc/expand_heredoc_line.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_heredoc_line.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: houabell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/06 10:55:05 by houabell          #+#    #+#             */
+/*   Updated: 2025/06/07 15:16:12 by houabell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+static char	*handle_variable_expansion(char *line, int *i, t_shell *shell)
+{
+	char	*var_name;
+	char	*var_value;
+	int		name_len;
+
+	(*i)++;
+	var_name = extract_var_name(&line[*i], &name_len);
+	if (var_name)
+	{
+		var_value = get_env_value(var_name, shell);
+		free(var_name);
+		*i += name_len;
+		return (var_value);
+	}
+	else if (line[*i] == '?')
+	{
+		var_value = ft_itoa(shell->exit_status);
+		(*i)++;
+		return (var_value);
+	}
+}
+
+static char	*append_literal_part(char *final_line, char *line, int start, int end)
+{
+	char	*part;
+
+	if (end > start)
+	{
+		part = ft_strndup(&line[start], end - start);
+		final_line = append_str(final_line, part);
+		free(part);
+	}
+	return (final_line);
+}
+
+static char	*process_dollar_sign(char *final_line, char *line, int *i, \
+		t_shell *shell)
+{
+	var_value = handle_variable_expansion(line, i, shell);
+	final_line = append_str(final_line, var_value);
+	free(var_value);
+	return (final_line);
+}
+
+char	*expand_heredoc_line(char *line, t_shell *shell)
+{
+	char	*final_line;
+	int		i;
+	int		start;
+
+	final_line = ft_strdup("");
+	i = 0;
+	start = 0;
+	while (line[i])
+	{
+		if (line[i] == '$' && is_allowed(line[i + 1]))
+		{
+			final_line = append_literal_part(final_line, line, start, i);
+			final_line = process_dollar_sign(final_line, line, &i, shell);
+			start = i;
+		}
+		else
+			i++;
+	}
+		final_line = append_literal_part(final_line ,line, start, i);
+		return (final_line);
+}
---
+++ src/heredoc/handle_heredocs.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_heredocs.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: houabell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/06 10:14:36 by houabell          #+#    #+#             */
+/*   Updated: 2025/06/06 10:35:30 by houabell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+static int	count_heredocs(t_shell *shell)
+{
+	t_token	*current;
+	int		count;
+
+	current = shell->tokens;
+	count = 0;
+	while (current)
+	{
+		if (current->type == TOKEN_HEREDOC)
+			count++;
+		current = current->next;
+	}
+	return (count);
+}
+
+static int	check_heredoc_limit(int count)
+{
+	if (count > MAX_HEREDOCS)
+	{
+		printf("minishell: maximum here-document count exceeded\n");
+		return (ERROR);
+	}
+	return (SUCCESS);
+}
+
+static int	process_heredoc_token(t_token *token, t_shell *shell)
+{
+	int	quotes_flag;
+
+	quotes_flag = token->next->original_delimiter_had_quotes;
+	if (read_heredoc_input(token->next->value, quotes_flag, shell) \
+			!= SUCCESS)
+		return (ERROR);
+	token->type = TOKEN_REDIR_IN;
+	free(token->value);
+	token->value = ft_strdup("<");
+	free(token->next->value);
+	token->next->value \
+		= ft_strdup(shell->heredoc_files[shell->heredoc_count - 1]);
+	return (SUCCESS);
+}
+
+static int	process_all_heredocs(t_shell *shell)
+{
+	t_token	*current;
+
+	current = shell->tokens;
+	while (current)
+	{
+		if (current->type == TOKEN_HEREDOC)
+		{
+			if (process_heredoc_token(current, shell) != SUCCESS)
+				return (ERROR);
+		}
+		current = current->next;
+	}
+	return (SUCCESS);
+}
+
+int	handle_heredocs(t_shell *shell)
+{
+	int	count;
+
+	count = count_heredocs(shell);
+	if (check_heredoc_limit(count) != SUCCESS)
+		return (ERROR);
+	return (process_all_heredocs(shell));
+}
